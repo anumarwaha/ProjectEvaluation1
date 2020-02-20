fea4b9e160653c7e53d7e0a3b4dc34120364408e
aniket003 <60145059+aniket003@users.noreply.github.com>
2020-02-20 10:11:08 +0530

    Aniket 1710991096

---
+++ Aniket Singla 1710991096/src/p1/Main.java
@@ -0,0 +1,18 @@
+package p1;
+import java.awt.Color;
+
+import javax.swing.JFrame;
+public class Main {
+
+	public static void main(String [] agrs)
+	{
+		JFrame obj=new JFrame();
+		Gameplay gameplay=new Gameplay();
+		obj.setBounds(10, 10, 905, 700);
+		obj.setBackground(Color.DARK_GRAY);
+		obj.setResizable(false);
+		obj.setVisible(true);
+		obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		obj.add(gameplay);
+	}
+}
